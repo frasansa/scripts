9aea55cc59b22fc44b3432679d943748d4b7591f
Francisco Sánchez Sáez <20833717H@FISABIO.ES>
2023-08-18 09:41:24 +0200

    add a fix for ggplot previews

--- Funciones.R
+++ Funciones.R
@@ -313,4 +313,13 @@ cargar_nombres <- function(cual, descrip = "") {
       comment ="")
 }
 
+# fix ggplots preview with camcorder
+camcorder::gg_record(
+  dir = "Images",
+  width = 12,
+  height = 12 * 9 / 16,
+  dpi = 300,
+  bg = 'white'
+  # Makes sure background of plot is actually white, not transparent
+)
 
