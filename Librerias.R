ab7feec38015022fd0f642c752438ef167214b8d
Francisco Sanchez-Saez <fcosanchezsaez@gmail.com>
2023-09-23 10:36:50 +0200

    remove blank line

--- Librerias.R
+++ Librerias.R
@@ -1,7 +1,6 @@
 # ejemplo instalación en local-----------------------------------------------
 # devtools::install(file.path("d:", "R_Projects", "Scripts", "Github_packages",
 #                              "DashboardExport-main"))
-
 # Sys.setenv(http_proxy="http://proxy.san.gva.es:8080/")
 # options(internet.info = 0)
 
